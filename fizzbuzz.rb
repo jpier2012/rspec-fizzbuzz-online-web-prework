cda1cd49147871c500c0549cc32748915b505ca9
James Pier <jpier2012@gmail.com>
2019-04-01 01:44:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz num
-  fb = ""
-  if num % 3 == 0
-    fb += "fizz"
-  end
-  if num % 5 == 0
-    fb += "buzz"
-  end
-  if fb == ""
+  if num % 3 == 0 && num % 5 == 0
+    "Fizzbuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else 
     nil
   end
-  fb
 end
 
 puts fizzbuzz(3)
