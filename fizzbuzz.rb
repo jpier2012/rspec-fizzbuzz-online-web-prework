1613b78c8eb6972d96a5ec5a78584fe602ffa689
James Pier <jpier2012@gmail.com>
2019-04-01 01:38:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz num
+  fb = ""
+  case num
+  when % 3 == 0
+    fb += "fizz"
+  when % 5 == 0
+    fb += "buzz"
+  end
+end
